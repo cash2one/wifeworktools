d7ede95c8ae252139b1aa47544699ef3d77757a1
ywdblog <ywdblog@gmail.com>
2016-09-05 15:23:14 +0800

    add

--- idfa/idfa.php
+++ idfa/idfa.php
@@ -10,16 +10,15 @@
  * 
  * fxdata/2016062.txt
  * fxdata/2016062_diff.txt
- * fxdata/sx/2016062
+ * fxdata/sx/2016062/* 筛选后的结果文件
  */
 error_reporting(E_ALL);
 set_time_limit(0);
 ini_set("memory_limit", "1000M");
 date_default_timezone_set('Europe/London');
 
-define('OS', 'linux');
-
-    include 'PHPExcel-1.8/Classes/PHPExcel/IOFactory.php';
+define('OS', 'windows');
+include 'PHPExcel-1.8/Classes/PHPExcel/IOFactory.php';
 
 function get_extension($file) {
     return substr($file, strrpos($file, '.') + 1);
@@ -29,11 +28,8 @@ function get_extension($file) {
 function pmkdir($dir) {
     $mkdir = "";
     $arr = explode("/", $dir);
-
     foreach ($arr as $v) {
-
         $mkdir .= $v . "/";
-
         if (!is_dir($mkdir))
             mkdir($mkdir);
     }
@@ -80,7 +76,6 @@ class dsfexcelfx {
     function importexcel() {
 
         $filearr = preaddir($this->srcdir);
-
         foreach ($filearr as $tmpfile) {
             $this->getexcel($tmpfile[0], $tmpfile[1]);
         }
@@ -172,28 +167,22 @@ class dmexcelfx {
         $srcdir = "srcdata/" . $this->action . "/" . $this->date;
         $dir = "descdata/{$this->action}/{$this->date}";
         pmkdir($dir);
-
         $this->srcdir = $srcdir;
         $this->descdir = $dir;
     }
 
-    //date标准格式应该 = 20160701
+    //date标准格式应该 = [20160701]
     public function parsedate($date) {
-
-
         $date = str_replace("年", "", $date);
         $date = str_replace("月", "", $date);
         $date = str_replace("日", "", $date);
+        $ym = substr($date, 0, 4) . "0" . substr($date, 4, 1);
+        $d = (substr($date, 5)) < 10 ? "0" . substr($date, 5) : substr($date, 5);
+        $str = $ym . $d;
 
- 
-         $ym = substr($date, 0, 4) . "0" . substr($date, 4, 1) ;
-         $d = (substr($date, 5))<10?"0".substr($date, 5):substr($date, 5);
-          $str =  $ym . $d   ;
-    
-        return $str ;
+        return $str;
 
         //   $date = substr($date,0,4) . "0"  , 
-
         $tmpdata = explode("/", $date);
         $d_y = $tmpdata[0];
         $d_m = $tmpdata[1];
@@ -256,10 +245,8 @@ class dmexcelfx {
                 $date = trim($v[$g_date]);
                 if ($date == "" || $sj == "")
                     continue;
-            
-               
+
                 $date = $this->parsedate($date);
-  
                 $this->data[$date][$sj] = 1;
             }
         }
@@ -269,9 +256,7 @@ class dmexcelfx {
 
         foreach ($this->data as $k => $v) {
             $file = $this->descdir . "/" . str_replace("-", "", $k) . ".txt";
-
             $fp = fopen($file, "w");
-
             foreach ($v as $kk => $vv) {
                 fwrite($fp, $kk . "\r\n");
             }
@@ -285,7 +270,6 @@ class dmexcelfx {
             $this->getexcel($tmpfile[0], $tmpfile[1]);
         }
     }
-
 }
 
 class fx {
@@ -296,16 +280,17 @@ class fx {
     public $fxdir = "";
     public $fxfile = "";
     public $fxdifffile = "";
+    public $excelfile = "";
     public $data = array();
 
     function __construct($date) {
         $this->dsfdir = "descdata/dsf/" . $date;
         $this->dmdir = "descdata/dm/" . $date;
-
         $this->fxdir = "fxdata";
         $this->sxdir = $this->fxdir . "/sx/" . $date;
         $this->fxfile = $this->fxdir . "/" . $date . "_fx.txt";
         $this->fxdifffile = $this->fxdir . "/" . $date . "_fxdiff.txt";
+        $this->excelfile = $this->fxdir . "/" . $date . ".xlsx";
         $this->date = $date;
     }
 
@@ -318,19 +303,18 @@ class fx {
     }
 
     function parse() {
- 
+
         $filearr = preaddir($this->dsfdir);
- 
         $this->data = array();
         $tmpfile = $this->fxdir . "/tmp.log";
         foreach ($filearr as $k => $v) {
             $dsffile_prex = $v[1];
-        
+
             $tmpdata = explode("_", $dsffile_prex);
             $g_date = $tmpdata[0];
             $g_type = $tmpdata[1];
-            $g_type = str_replace(".txt", "",$g_type );
-        
+            $g_type = str_replace(".txt", "", $g_type);
+
             $dsffile = $v[0] . "/" . $v[1];
             $dmfile = $this->dmdir . "/" . $g_date . ".txt";
             $sxfile = $this->sxdir . "/" . $g_date . $g_type . ".txt"; //筛选文件
@@ -356,6 +340,62 @@ class fx {
         unlink($fxtmp);
     }
 
+    function saveexcel() {
+
+        $file = $this->excelfile;
+        if (!file_exists($file)) {
+            $objPHPExcel = new PHPExcel();
+            $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+            $objWriter->save($file);
+            $objPHPExcel = NULL;
+            $objWriter = NULL;
+        }
+
+        $sheet = "x1";
+        $objPHPExcel = PHPExcel_IOFactory::load($file);
+        $count = $objPHPExcel->getSheetCount();
+        $objPHPExcel->createSheet();
+        $objPHPExcel->setActiveSheetIndex($count);
+        $objPHPExcel->getActiveSheet()->setTitle($sheet);
+
+        $dataarr = file($this->fxdifffile);
+        $row = 1;
+        foreach ($dataarr as $v) {
+
+            $v = trim($v);
+            if ($v == "")
+                continue;
+            $tmpstr = explode("_", $v);
+
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0, $row, $tmpstr[1]);
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, $row, $tmpstr[2]);
+            $row++;
+        }
+
+        $sheet = "x2";
+        $objPHPExcel->createSheet();
+        $objPHPExcel->setActiveSheetIndex($count + 1);
+        $objPHPExcel->getActiveSheet()->setTitle($sheet);
+
+        $dataarr = file($this->fxfile);
+        $row = 1;
+        foreach ($dataarr as $v) {
+
+            $v = trim($v);
+            if ($v == "")
+                continue;
+            $tmpstr = explode("_", $v);
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0, $row, $tmpstr[0]);
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, $row, $tmpstr[1]);
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, $row, $tmpstr[2]);
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, $row, $tmpstr[3]);
+            $row++;
+        }
+
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save($file);
+    }
+
     function exportdata() {
         $arr = file($this->fxfile);
         $this->data = array();
@@ -363,38 +403,35 @@ class fx {
             $t = explode("_", $v);
             if ($t[2] / $t[3] < 0.95) {
                 $file = $this->sxdir . "/" . $t[0] . $t[1] . ".txt";
-echo $file . "\r\n";
-		$tmp = file($file);
+
+                $tmp = file($file);
                 foreach ($tmp as $v) {
                     $v = trim($v);
                     if ($v != "")
-                        $this->data[] = array($t[0],$t[1],$v);
-
-                    //fwrite($fp, $t[1] . "_" . $t[2] . "_" . $v . "\r\n");
+                        $this->data[] = array($t[0], $t[1], $v);
                 }
             }
         }
- 
-	$fp = fopen($this->fxdifffile, "w");
-	if ($fp) {
-		foreach ($this->data as $v) {
-			fwrite($fp,implode("_",$v) . "\r\n");
-		}
-		fclose($fp);
-	}
-    }
 
+        $fp = fopen($this->fxdifffile, "w");
+        if ($fp) {
+            foreach ($this->data as $v) {
+                fwrite($fp, implode("_", $v) . "\r\n");
+            }
+            fclose($fp);
+        }
+    }
 }
 
 $action = $argv[1];
 $date = $argv[2]; //日期
 
-//$action = "dm";
-//$date = "2016083";
+$action = "export";
+$date = "2016083";
 
 switch ($action) {
     case "dsf":
-        //文件名应该是 20160607这样的前缀
+        //文件名应该是 [20160607]这样的前缀
         $obj = new dsfexcelfx($action, $date);
         $obj->initdir();
         $obj->importexcel();
@@ -402,26 +439,29 @@ switch ($action) {
         break;
 
     case "dm":
-        //excel 里面日期的标准格式是20160607,假如不是需要做些修改
+        //excel 里面日期的标准格式是20160607,假如不是需要做些修改,具体是修改parsedate函数
+        //excel 的sheet 头应该是 [idfa]和[date]
         $obj = new dmexcelfx($action, $date);
         $obj->initdir();
         $obj->importexcel();
         $obj->writedata();
         break;
 
+    //比较dm和disf的数据,并生成一个文本文件
     case "fx":
         $obj = new fx($date);
         $obj->initdir();
-	echo "start";
         $obj->parse();
-
         $obj->writedata();
         break;
+
+    //将结果文件导出到excel中
     case "export":
 
         $obj = new fx($date);
         $obj->initdir();
-	$obj->exportdata();
-  break;
+        $obj->exportdata();
+        $obj->saveexcel();
+        break;
 }
         
